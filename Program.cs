e96cec2e3604aa215ccc4a86752a94820c0478b9
Евгений Кучма <desferto87@gmail.com>
2024-07-08 01:03:03 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkФrray_value_shift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int userInputShitLeft = 0;
+            int minValue = 1;
+            int maxValue = 10;
+            int digitalArraySize = 10;
+            int temp = 0;
+
+            int[] digitalArray = new int[digitalArraySize];
+
+            for (int i = 0; i < digitalArray.Length; i++)
+            {
+                digitalArray[i] = random.Next(minValue, maxValue);
+                Console.Write(digitalArray[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the value of the desired shift: ");
+            userInputShitLeft = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = 0; j < userInputShitLeft; j++)
+            {
+                temp = digitalArray[0];
+                for (int i = 0; i < digitalArray.Length - 1; i++)
+                {
+                    digitalArray[i] = digitalArray[i + 1];
+
+                }
+
+                digitalArray[digitalArraySize - 1] = temp;
+            }
+
+            for (int i = 0; i < digitalArraySize; i++)
+            {
+                Console.Write(digitalArray[i] + " ");
+            }
+        }
+    }
+}
